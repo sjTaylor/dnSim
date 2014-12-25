13cbb8bb33e2e124462e786f65a44376b6fa7ab9
Stuart Taylor <stuarttaylor7@gmail.com>
2014-12-25 11:39:39 -0600

    Decided to go with xml for data
    Currently in the process of making the Abyss Walker class.

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import snip
+import xml.etree.ElementTree as ET
+tree = ET.parse('test.xml')
+root = tree.getroot()
 
-a = snip.twoD(4,4,0)
-print(a)
+for country in root.findall('country'):
+   rank = country.find('rank').text
+   name = country.get('name')
+   print(name, rank)
